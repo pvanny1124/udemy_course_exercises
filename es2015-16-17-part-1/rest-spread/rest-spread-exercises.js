29719dc62f7f0b3236d2da1ab19d087fecbba7f9
Patrick Vanegas <pvanny1124@gmail.com>
2018-07-19 01:14:42 -0400

    Update rest-spread-exercises.js

--- es2015-16-17-part-1/rest-spread/rest-spread-exercises.js
+++ es2015-16-17-part-1/rest-spread/rest-spread-exercises.js
@@ -1,17 +1,36 @@
 // spread
-function smallestValue(...args) {}
+function smallestValue(...args){
+  return Math.min(...args)
+}
 
 // placeInMiddle([1,2,6,7],[3,4,5])
-function placeInMiddle(arr, vals) {}
+function placeInMiddle(arr, vals){
+  let mid = Math.floor(arr.length/2)
+  arr.splice(mid,0,...vals)
+  return arr;
+}
 
 // rest
-function joinArrays(...args) {}
+function joinArrays(...args){
+  return args.reduce((acc, next) => acc.concat(next), [])
+}
 
 // rest
-function sumEvenArgs(...args) {}
+function sumEvenArgs(...args){
+  return args.reduce((acc, next) => next % 2 === 0 ? acc += next : acc, 0)
+}
 
 // rest
-function flip(fn, thisArg, ...outerArgs) {}
+function flip(fn, thisArg, ...outerArgs){
+  return function(...innerArgs){
+    let allArgs = outerArgs.concat(innerArgs).slice(0, fn.length)
+    return fn.apply(thisArg, allArgs.reverse());
+  }
+}
 
 // rest + spread
-function bind(fn, thisArg, ...outerArgs) {}
+function bind(fn, thisArg, ...outerArgs){
+  return function(...innerArgs){
+    return fn.apply(thisArg, [...outerArgs, ...innerArgs])
+  }
+}
